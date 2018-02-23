b581c458e103c0e6c2fbc14958e8e8245ab73255
jetsontwo <jetsontwo@gmail.com>
2018-02-23 12:31:47 -0800

    Made Options Reflect Previously Set Controls
    Made the switch in the options page reflect what the player had
    previously set.

--- Assets/Scripts/Setup/Option_Controller.cs
+++ Assets/Scripts/Setup/Option_Controller.cs
@@ -6,10 +6,18 @@ public class Option_Controller : MonoBehaviour {
 
     public bool reset_all;
 
+    [Header("Joystick Controls")]
+    public GameObject Joystick_On;
+    public GameObject Joystick_Off;
+
 	void Start()
     {
         if (reset_all)
             Reset_All();
+
+        //These set all the controls to visual reflect what the player had set
+        Check_Control_Preference();
+
     }
 
     public void Reset_All()
@@ -22,4 +30,15 @@ public class Option_Controller : MonoBehaviour {
         PlayerPrefs.SetInt("Joystick", use ? 1 : 0);
     }
 
+    private void Check_Control_Preference()
+    {
+        if (PlayerPrefs.HasKey("Joystick"))
+        {
+            if (PlayerPrefs.GetInt("Joystick") == 1)
+            {
+                Joystick_Off.SetActive(true);
+                Joystick_On.SetActive(false);
+            }
+        }
+    }
 }
