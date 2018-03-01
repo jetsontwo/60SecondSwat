e2bc4c810d2b0dc9becdd805bf3a540e5601ecb1
jetsontwo <jetsontwo@gmail.com>
2018-03-01 00:25:35 -0800

    Added Revamped Shop
    Used new UI Assest pack to create a nicer looking store interface for
    guns, armor and coins.

---
+++ Assets/Scripts/UI/Shop_Controller.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Shop_Controller : MonoBehaviour {
+
+    [Header("Content")]
+    public GameObject gunsTab;
+    public GameObject armorTab;
+    public GameObject coinsTab;
+
+    [Header("Buttons")]
+    public Image gunsButton;
+    public Image armorButton;
+    public Image coinsButton;
+    public Sprite onSprite;
+    public Sprite offSprite;
+
+    private GameObject curTab;
+    private Image curSprite;
+
+    void Start()
+    {
+        curTab = gunsTab;
+        curSprite = gunsButton;
+    }
+
+    public void switchTab(GameObject newTab)
+    {
+        curTab.SetActive(false);
+        curTab = newTab;
+        curTab.SetActive(true);
+    }
+
+    public void switchSprite(Image newSprite)
+    {
+        curSprite.sprite = offSprite;
+        curSprite = newSprite;
+        curSprite.sprite = onSprite;
+    }
+}
